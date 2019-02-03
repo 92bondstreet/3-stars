42141b4de7ebd00478fcef0115d24d8b902c5fa5
Yassine AZZOUT <92bondstreet@gmail.com>
2019-02-03 10:34:20 +0100

    WIP search

--- src/components/HitsStats/index.js
+++ src/components/HitsStats/index.js
@@ -1,13 +1,25 @@
 import * as React from 'react';
 import { connectStateResults } from 'react-instantsearch-dom';
 
-import { UikContentTitle, UikTag, UikWidget } from '@uik';
+import { UikContentTitle, UikWidget } from '@uik';
 
 import styles from './hits-stats.module.scss';
 
-const Stats = connectStateResults(({searchResults}) => {
-  const facets = searchResults && searchResults.facets || [];
-  const {data} = facets.find(item => item.name === 'type') || {'data': {}};
+/**
+ * Get data from a facet name
+ * @param  {Object} results - algolia search results
+ * @param  {String} name - of facet
+ * @return {Object} facet's data
+ */
+const getFacet = (results = {}, name = '') => {
+  const facets = (results && results.facets) || [];
+  const { data } = facets.find(item => item.name === name) || { data: {} };
+
+  return data;
+};
+
+const Stats = connectStateResults(({ searchResults }) => {
+  const data = getFacet(searchResults, 'type');
 
   return (
     <UikWidget margin>
@@ -15,13 +27,13 @@ const Stats = connectStateResults(({searchResults}) => {
         <div className={styles.box}>
           <UikContentTitle>Newsletter posts</UikContentTitle>
           <div className={styles.boxContent}>
-            <span className={styles.boxValue}>📱 {data.newsletter}</span>
+            <span className={styles.boxValue}>📱 {data.newsletter || 0}</span>
           </div>
         </div>
         <div className={styles.box}>
           <UikContentTitle>Podcats</UikContentTitle>
           <div className={styles.boxContent}>
-            <span className={styles.boxValue}>🎙️ {data.podcast}</span>
+            <span className={styles.boxValue}>🎙️ {data.podcast || 0}</span>
           </div>
         </div>
       </div>
