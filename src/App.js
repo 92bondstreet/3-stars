5f9a7f235fb332302b1530587d27ce7ad11465fd
Yassine AZZOUT <92bondstreet@gmail.com>
2019-02-03 22:56:14 +0100

    feat(mobile): make navigation available on mobile

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import Header from './components/Header';
-import Hits from './components/Hits';
-import Sources from './components/Sources';
 import { Configure, InstantSearch } from 'react-instantsearch-dom';
-
-import { UikContainerHorizontal, UikContainerVertical } from '@uik';
-import { ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY } from './constants';
-
-import cls from './App.module.scss';
 import '@uik/styles.css'; //eslint-disable-line
 import './algolia.scss';
+import styles from './app.module.scss';
+import Container from './components/Container';
+import { ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY } from './constants';
 
 const searchClient = algoliasearch(ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY);
 
 class App extends Component {
   render() {
     return (
-      <div className={cls.app}>
+      <div className={styles.app}>
         <InstantSearch searchClient={searchClient} indexName="threestars">
           <Configure facets={['type']} />
-          <UikContainerVertical>
-            <Header />
-            <UikContainerHorizontal>
-              <Sources attribute="domain" />
-              <Hits />
-            </UikContainerHorizontal>
-          </UikContainerVertical>
+          <Container />
         </InstantSearch>
       </div>
     );
---
+++ src/components/Container.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Header from './Header';
+import Hits from './Hits';
+import Sources from './Sources';
+import classnames from 'classnames';
+import {
+  UikTabContainer,
+  UikTabItem,
+  UikContainerHorizontal,
+  UikContainerVertical,
+  Uikon,
+} from '@uik';
+
+import styles from './container.module.scss';
+
+const tabItems = [
+  {
+    className: 'Menu',
+    icon: 'gallery_grid_view',
+    content: <Uikon>gallery_grid_view</Uikon>,
+  },
+  {
+    id: 'centerIcon',
+    className: 'Home',
+    icon: 'home',
+    content: <Uikon>home</Uikon>,
+  },
+];
+
+class Container extends React.PureComponent<{
+  location: Object,
+}> {
+  static getDerivedStateFromProps(props, state) {
+    return {
+      ...state,
+      activeContent: state.activeContent,
+    };
+  }
+
+  state = {
+    activeContent: 'Home',
+  };
+
+  setActiveContent = content => () => {
+    this.setState({
+      activeContent: content,
+    });
+  };
+
+  render() {
+    const { activeContent } = this.state;
+
+    return (
+      <UikContainerVertical>
+        <Header />
+        <UikTabContainer className={styles.mobileNavigation}>
+          {tabItems.map(item => (
+            <UikTabItem
+              key={classnames(styles.mobileItem, item.className)}
+              className={classnames(styles.mobileItem, {
+                active: activeContent === item.className,
+              })}
+              onClick={this.setActiveContent(item.className)}
+              size="smaller"
+            >
+              <Uikon>{item.id === 'centerIcon' ? 'home' : item.icon}</Uikon>
+            </UikTabItem>
+          ))}
+        </UikTabContainer>
+        <UikContainerHorizontal
+          className={classnames(styles.contentContainer, {
+            [styles[activeContent]]: activeContent,
+          })}
+        >
+          <Sources attribute="domain" />
+          <div className={styles.content}>
+            <Hits />
+          </div>
+        </UikContainerHorizontal>
+      </UikContainerVertical>
+    );
+  }
+}
+
+export default Container;
+
+/*
+ */
