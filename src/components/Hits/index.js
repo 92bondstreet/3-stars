24e7a6acdcdabfa266ffe77711a3ff5b9653faa4
Yassine AZZOUT <92bondstreet@gmail.com>
2019-02-04 18:04:07 +0100

    feat(infinite): move from hits to infinite

--- src/components/Hits/index.js
+++ src/components/Hits/index.js
@@ -1,33 +1,71 @@
-import * as React from 'react';
-import { connectHits } from 'react-instantsearch-dom';
+import React, { Component } from 'react';
+import { connectInfiniteHits } from 'react-instantsearch-dom';
 import Header from './Header';
 import Highlight from './Highlight';
+import PropTypes from 'prop-types';
 import Stats from './Stats';
 import { UikContainerVertical, UikScrollArea, UikDivider } from '@uik';
 import Wrapper from './Wrapper';
 
 import styles from './hits.module.scss';
 
-const Hits = connectHits(({ hits }) => (
-  <UikContainerVertical className={styles.page}>
-    <UikScrollArea className={styles.mainContent}>
-      <Stats />
-      {hits.map(hit => (
-        <Wrapper key={hit.objectID}>
-          <Header
-            date={hit.date}
-            domain={hit.domain}
-            source={hit.source}
-            title={hit.title}
-            type={hit.type}
-            url={hit.url}
-          />
-          <Highlight hit={hit} attribute="tldr" />
-          <UikDivider />
-        </Wrapper>
-      ))}
-    </UikScrollArea>
-  </UikContainerVertical>
-));
+class InfiniteHits extends Component {
+  static propTypes = {
+    hits: PropTypes.arrayOf(PropTypes.object).isRequired,
+    hasMore: PropTypes.bool.isRequired,
+    refine: PropTypes.func.isRequired,
+  };
 
-export default Hits;
+  sentinel = null;
+
+  onSentinelIntersection = entries => {
+    const { hasMore, refine } = this.props;
+
+    entries.forEach(entry => {
+      if (entry.isIntersecting && hasMore) {
+        refine();
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.observer = new IntersectionObserver(this.onSentinelIntersection);
+
+    this.observer.observe(this.sentinel);
+  }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
+  }
+
+  render() {
+    const { hits } = this.props;
+    return (
+      <UikContainerVertical className={styles.page}>
+        <UikScrollArea className={styles.mainContent}>
+          <Stats />
+          {hits.map(hit => (
+            <Wrapper key={hit.objectID}>
+              <Header
+                date={hit.date}
+                domain={hit.domain}
+                source={hit.source}
+                title={hit.title}
+                type={hit.type}
+                url={hit.url}
+              />
+              <Highlight hit={hit} attribute="tldr" />
+              <UikDivider />
+            </Wrapper>
+          ))}
+          <div
+            className="ais-InfiniteHits-sentinel"
+            ref={c => (this.sentinel = c)}
+          />
+        </UikScrollArea>
+      </UikContainerVertical>
+    );
+  }
+}
+
+export default connectInfiniteHits(InfiniteHits);
